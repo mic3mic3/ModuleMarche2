150d91514e370bd87c4960e484bf054f92be64e4
mic3mic3 <mic3mic3@hotmail.com>
2015-04-06 23:03:58 -0400

    Classe statique Fichier: Entrées/sorties par fichier.
    J'ai extrait le traitement d'écriture et lecture dans des fichiers vers
    des méthodes dans la classe Fichier.  Elle ne sont pas encore utilisées
    ni testées pour l'instant.  Il faudra le faire un par un et m'assurer
    que ça fonctionne bien.

---
+++ ModuleMarche/ModuleMarche/Fichier.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <fstream> //Pour les opérations dans un fichier
+#include <vector>
+
+#include "Fichier.h"
+
+using namespace std;
+
+const char Fichier::CC_VALUE_SEPARATOR = ';';
+const string Fichier::CC_LINE_SEPARATOR = "\n";
+
+Fichier::Fichier(void)
+{
+}
+
+Fichier::~Fichier(void)
+{
+}
+
+///<summary>Prépare la mise en forme des données avant l'écriture dans un fichier. (Valeurs séparées par des «;», entrées sur des lignes différentes, etc.)</summary>
+string Fichier::getContenuFormatePourPourEcriture(vector<vector<string>>* poEntrees)
+{
+	if (poEntrees == nullptr)
+		return "";
+
+	string lsContenu = "";
+	for each (vector<string> loEntree in *poEntrees)
+	{
+		for each (string lsParametre in loEntree)
+		{
+			lsContenu += lsParametre + CC_VALUE_SEPARATOR;
+		}
+
+		// À la fin de la ligne du fichier, on remplace le dernier caractère de séparation de valeurs «;» par une caractère de fin de ligne «\n»
+		if (lsContenu[lsContenu.length() - 1] = CC_VALUE_SEPARATOR)
+		{
+			lsContenu.replace(lsContenu.end(), lsContenu.end(), CC_LINE_SEPARATOR);
+		}
+	}
+	return lsContenu;
+}
+
+///<summary>Renvoie le contenu du fichier. Chaque ligne sera un vecteur de chaînes, dont chaque valeur entre «;» sera une chaîne.</summary>
+vector<vector<string>>* Fichier::getContenu(string psNomCompletFichier)
+{
+	// Ouverture du fichier
+	fstream loFStream(psNomCompletFichier, ios::in);
+	if (!loFStream.is_open())
+		return nullptr;
+
+	// Lecture de chaque ligne du fichier
+	string ligne;
+	vector<vector<string>>* loEntrees = new vector<vector<string>>();
+	vector<string> loEntree;
+	while (getline(loFStream, ligne) && ligne.length() != NULL)
+	{
+		// Lecture d'une ligne
+		unsigned int nbPtsVirgs = 0;
+		loEntree = vector<string>();
+		for (size_t cpt = 0; cpt < ligne.length(); cpt++)
+		{
+			if (ligne[cpt] == CC_VALUE_SEPARATOR)
+			{
+				nbPtsVirgs++;
+			}
+			else
+			{
+				if (loEntree.size() < nbPtsVirgs)
+					loEntree.insert(loEntree.end(), "");
+
+				loEntree.at(nbPtsVirgs) += ligne[cpt];
+			}
+		}
+		loEntrees->insert(loEntrees->end(), loEntree);
+	}
+	return loEntrees;
+}
+
+///<summary>Remplace le contenu d'un fichier par les entrées fournies en paramètre.</summary>
+bool Fichier::setContenu(string psNomCompletFichier, vector<vector<string>>* poEntrees)
+{
+	fstream loFStream(psNomCompletFichier, ios::app);
+	if (!loFStream.is_open())
+		return false;
+
+	loFStream << getContenuFormatePourPourEcriture(poEntrees);
+	loFStream.close();
+	return true;
+}
---
+++ ModuleMarche/ModuleMarche/Fichier.h
@@ -0,0 +1,26 @@
+#ifndef FICHIER_H
+#define FICHIER_H
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+///<summary>Gestion des entrées/sorties de l'application via des fichiers.</summary>
+class Fichier
+{
+public:
+	static const char CC_VALUE_SEPARATOR;
+	static const string CC_LINE_SEPARATOR;
+
+	Fichier(void);
+	~Fichier(void);
+
+
+	static string getContenuFormatePourPourEcriture(vector<vector<string>>*);
+	static vector<vector<string>>* getContenu(string);
+	static bool setContenu(string, vector<vector<string>>*);
+};
+
+#endif
