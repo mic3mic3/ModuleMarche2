5f9393b195d9741cb1d4f5050c7d79336170911a
axof <mat_38@msn.com>
2015-04-20 11:01:39 -0400

    Return oublié

--- ModuleMarche/ModuleMarche/Simulation.cpp
+++ ModuleMarche/ModuleMarche/Simulation.cpp
@@ -18,13 +18,14 @@ void Simulation::miseAJour()
 	if (chanceClient == 1)
 	{
 		DWORD* idThread = new DWORD();
-		threads.push_back(CreateThread(0, 0, appelClient, new ClientSim(), 0, idThread);
+		//threads.push_back(CreateThread(0, 0, appelClient, new ClientSim(), 0, idThread);
 	}
 }
 
 DWORD WINAPI Simulation::appelClient(LPVOID client)
 {
 	((ClientSim*)client)->miseAJour(mutex);
+	return 0;
 }
 
 int Simulation::getHeures()
